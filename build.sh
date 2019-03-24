5e4ce3c62ddc7d5c05bc9b9c10be3a4db9aff56c
Loki Coyote <lokkju@gmail.com>
2019-03-24 01:31:18 -0700

    created an output directory

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
-qmake CONFIG+=release PREFIX=/usr
+mkdir -p debug
+cd debug
+qmake CONFIG+=release PREFIX=/usr ../SPFlashToolAPCore.pro
 make
 make INSTALL_ROOT=appdir install
 find appdir/
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 export VERSION=5.1728
-LD_LIBRARY_PATH=`pwd`./Lib /opt/linuxdeployqt --appimage-extract-and-run appdir/usr/share/applications/*.desktop -appimage
-
+LD_LIBRARY_PATH=`pwd`/../Lib /opt/linuxdeployqt --appimage-extract-and-run appdir/usr/share/applications/*.desktop -appimage
+cd ..
